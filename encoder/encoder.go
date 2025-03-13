572c678b3ef881094a99ab79208845a8ed977313
b1ron <goldingbyron@gmail.com>
2025-03-14 00:37:33 +0100

    optimizations for scanning tokens

--- encoder/encoder.go
+++ encoder/encoder.go
@@ -2,6 +2,7 @@ package encoder
 
 // optimizations for the encoder package
 // 1. scan tokens in the input string and build the frequency map in parallel
+// 2. ...
 
 // Table represents a BPE translation table.
 type Table struct {
