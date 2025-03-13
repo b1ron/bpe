306514ffeec6f9eb257783373ff09efa4a3358e9
b1ron <goldingbyron@gmail.com>
2025-03-14 00:28:15 +0100

    figure out optimizations for token scanning

--- encoder/encoder.go
+++ encoder/encoder.go
@@ -1,7 +1,7 @@
 package encoder
 
 // optimizations for the encoder package
-// 1. scan tokens in the input string and build a map of tokens to their frequency can be done in parallel
+// 1. scan tokens in the input string and build the frequency map in parallel
 
 // Table represents a BPE translation table.
 type Table struct {
