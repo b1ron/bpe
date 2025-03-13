4a1486fdc6bc4fd3b0da899239abe62a619abb43
b1ron <goldingbyron@gmail.com>
2025-03-13 22:47:52 +0100

    comment in test and add wikipedia link

--- encoder/encoder_test.go
+++ encoder/encoder_test.go
@@ -7,6 +7,14 @@ func TestXxx(t *testing.T) {
 		text string
 		want Table
 	}{
+		/*
+			XdXac
+			X=ZY
+			Y=ab
+			Z=aa
+			this data cannot be compressed further by byte pair encoding because there are no repeated pairs of characters
+			https://en.wikipedia.org/wiki/Byte_pair_encoding
+		*/
 		{"aaabdaaabac", Table{m: map[string]string{"Z": "aa", "Y": "ab", "X": "ZY"}, encoded: "XdXac"}},
 	}
 	for _, test := range tests {
